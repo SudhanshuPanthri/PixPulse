49af6223fbac20f4058a17f8150418cebfe76938
sudhanshupanthri <panthrisudhanshu666@gmail.com>
2025-04-16 21:01:56 +0530

    Code Cleanup

--- components/step-by-step-section.tsx
+++ components/step-by-step-section.tsx
@@ -30,7 +30,7 @@ export function StepByStepSection() {
       value: "step3",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-          <p>Grounding DINO analyse and Detect contents</p>
+          <p>Facebook detr-resnet-50 detect contents</p>
           <DummyContent />
         </div>
       ),
@@ -45,16 +45,6 @@ export function StepByStepSection() {
         </div>
       ),
     },
-    // {
-    //   title: "Random",
-    //   value: "random",
-    //   content: (
-    //     <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
-    //       <p>Random tab</p>
-    //       <DummyContent />
-    //     </div>
-    //   ),
-    // },
   ];
 
   return (
@@ -64,14 +54,15 @@ export function StepByStepSection() {
   );
 }
 
+//TODO-Pass imageurl as prop to this component
 const DummyContent = () => {
   return (
     <Image
-      src="/noise.webp"
+      src="/image.png"
       alt="dummy image"
       width="1000"
       height="1000"
-      className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
+      className="object-contain object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
     />
   );
 };
